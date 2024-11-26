ed97d5a8b6a0338a7cc2caffca083da54461108b
YKwillway <yuchan0708bicky@gmail.com>
2024-11-27 06:59:12 +0900

    従業員の登録・更新機能を追加する

--- dev-spring-boot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ dev-spring-boot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,7 +6,9 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,6 +25,26 @@ public class EmployeeController {
     public String listEmployees(Model theModel) {
         List<Employee> theEmployees = employeeService.findAll();
         theModel.addAttribute("employees", theEmployees);
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showForm(Model theModel) {
+        Employee theEmployee = new Employee();
+        theModel.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        Employee theEmployee = employeeService.findById(theId);
+        theModel.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
+
+    @RequestMapping("/save")
+    public String saveEmployee(@ModelAttribute Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return "redirect:list";
     }
 }
